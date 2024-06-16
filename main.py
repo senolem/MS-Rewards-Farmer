08b551db10e3beeb9afc4c71b9827065e1dbf812
Cal Williams <9409256+cal4@users.noreply.github.com>
2024-06-16 18:57:43 -0400

    Configure apprise summary via command line arg

--- main.py
+++ main.py
@@ -257,9 +257,13 @@ def executeBot(currentAccount: Account, args: argparse.Namespace):
     logging.info(
         f"[POINTS] You are now at {utils.formatNumber(accountPointsCounter)} points !"
     )
-    appriseSummary = AppriseSummary[
-        utils.config.get("apprise", {}).get("summary", AppriseSummary.always.name)
-    ]
+    appriseSummary: AppriseSummary
+    if args.apprise_summary is not None:
+        appriseSummary = args.apprise_summary
+    else:
+        appriseSummary = AppriseSummary[
+            utils.config.get("apprise", {}).get("summary", AppriseSummary.always.name)
+        ]
     if appriseSummary == AppriseSummary.always:
         goalNotifier = ""
         if goalPoints > 0:
