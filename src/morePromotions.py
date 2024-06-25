ea617bf40c2bdfce8c64cd25c4400246d86ed33a
Cal Williams <9409256+cal4@users.noreply.github.com>
2024-06-25 00:08:59 -0400

    Handle some special url rewards; invert condition to help with indentation; continue versus return if exception

--- src/morePromotions.py
+++ src/morePromotions.py
@@ -12,38 +12,48 @@ class MorePromotions:
     def completeMorePromotions(self):
         # Function to complete More Promotions
         logging.info("[MORE PROMOS] " + "Trying to complete More Promotions...")
-        self.browser.utils.goHome()
-        morePromotions = self.browser.utils.getDashboardData()["morePromotions"]
-        i = 0
+        morePromotions: list[dict] = self.browser.utils.getDashboardData()[
+            "morePromotions"
+        ]
         for promotion in morePromotions:
             try:
-                i += 1
+                # Open the activity for the promotion
                 if (
-                    promotion["complete"] is False
-                    and promotion["pointProgressMax"] != 0
+                    promotion["complete"] is not False
+                    or promotion["pointProgressMax"] == 0
                 ):
-                    # Open the activity for the promotion
-                    self.activities.openMorePromotionsActivity(i)
-                    if promotion["promotionType"] == "urlreward":
-                        # Complete search for URL reward
-                        self.activities.completeSearch()
-                    elif (
-                        promotion["promotionType"] == "quiz"
-                        and promotion["pointProgress"] == 0
-                    ):
-                        # Complete different types of quizzes based on point progress max
-                        if promotion["pointProgressMax"] == 10:
-                            self.activities.completeABC()
-                        elif promotion["pointProgressMax"] in [30, 40]:
-                            self.activities.completeQuiz()
-                        elif promotion["pointProgressMax"] == 50:
-                            self.activities.completeThisOrThat()
-                    else:
-                        # Default to completing search
-                        self.activities.completeSearch()
+                    continue
+                self.activities.openMorePromotionsActivity(
+                    morePromotions.index(promotion)
+                )
+                if promotion["promotionType"] == "urlreward":
+                    if promotion["title"] == "Search the lyrics of a song":
+                        self.browser.webdriver.get(
+                            "https://www.bing.com/search?q=black+sabbath+supernaut+lyrics"
+                        )
+                    elif promotion["title"] == "Translate anything":
+                        self.browser.webdriver.get(
+                            "https://www.bing.com/search?q=translate+pencil+sharpener+to+spanish"
+                        )
+                    # Complete search for URL reward
+                    self.activities.completeSearch()
+                elif (
+                    promotion["promotionType"] == "quiz"
+                    and promotion["pointProgress"] == 0
+                ):
+                    # Complete different types of quizzes based on point progress max
+                    if promotion["pointProgressMax"] == 10:
+                        self.activities.completeABC()
+                    elif promotion["pointProgressMax"] in [30, 40]:
+                        self.activities.completeQuiz()
+                    elif promotion["pointProgressMax"] == 50:
+                        self.activities.completeThisOrThat()
+                else:
+                    # Default to completing search
+                    self.activities.completeSearch()
             except Exception:  # pylint: disable=broad-except
                 logging.error("[MORE PROMOS] Error More Promotions", exc_info=True)
                 # Reset tabs in case of an exception
                 self.browser.utils.resetTabs()
-                return
+                continue
         logging.info("[MORE PROMOS] Completed More Promotions successfully !")
