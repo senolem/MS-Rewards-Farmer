92d272088d5e5018dca0600f3ac155ec322c147c
Cal Williams <9409256+cal4@users.noreply.github.com>
2024-10-23 00:09:54 -0400

    Fix issue where page load timed out

--- src/browser.py
+++ src/browser.py
@@ -102,6 +102,7 @@ class Browser:
         options.add_argument("--disable-features=PrivacySandboxSettings4")
         options.add_argument("--disable-http2")
         options.add_argument("--disable-search-engine-choice-screen")  # 153
+        options.page_load_strategy = 'eager'
 
         seleniumwireOptions: dict[str, Any] = {"verify_ssl": False}
 
